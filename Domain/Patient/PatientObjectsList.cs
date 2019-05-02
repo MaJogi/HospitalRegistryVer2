6ec16c90d9acb8a960f2fca8fbc91816d74b7549
MaJogi <47116426+MaJogi@users.noreply.github.com>
2019-05-02 22:35:07 +0300

    Added PatientObjectsList and PatientViewModelList

---
+++ Domain/Patient/PatientObjectsList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Open.Data.Patient;
+
+namespace Open.Domain.Patient
+{
+    public class PatientObjectsList : List<PatientObject>
+    {
+        public PatientObjectsList(IEnumerable<PatientDbRecord> l)
+        {
+            foreach (var e in l) { Add(new PatientObject(e));}
+        }
+    }
+}
---
+++ Facade/Patient/PatientViewModelsList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Open.Domain.Patient;
+
+namespace Open.Facade.Patient
+{
+    public class PatientViewModelsList : List<PatientViewModel>
+    {
+        public PatientViewModelsList(IEnumerable<PatientObject> l)
+        {
+            foreach (var e in l)
+            {
+                Add(PatientViewModelFactory.Create(e));
+            }
+        }
+    }
+ 
+}
