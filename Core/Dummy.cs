6b5aefa35dd9b21d45223e5ecf4112ada069fb8b
Jegor Laptev <jelapt@ttu.ee>
2019-05-23 15:56:29 +0300

    <testing

---
+++ Core/Dummy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open.Core
+{
+    public class Dummy
+    {
+        public string Aaa { get; set;  }
+    }
+}
---
+++ Tests/Core/DummyTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open.Tests.Core
+{
+    [TestClass]
+    public class DummyTests
+    {
+        [TestMethod]
+        public void CanCreateTest()
+        {
+            Assert.IsNotNull(new DummyTests());
+        }
+    }
+}
