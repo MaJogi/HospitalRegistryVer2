73b3159fa0eaa3b1b9ed33cb3d3cdc2909cee682
Jegor Laptev <jelapt@ttu.ee>
2019-05-23 17:42:08 +0300

    Hard tests

---
+++ Tests/Aids/PublicBindingFlagsForTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Open.Tests.Aids
+{
+    [TestClass]
+    public class PublicBindingFlagsForTests
+    {
+        private const BindingFlags p = BindingFlags.Public;
+        private const BindingFlags i = BindingFlags.Instance;
+        private const BindingFlags s = BindingFlags.Static;
+        private const BindingFlags d = BindingFlags.DeclaredOnly;
+        private Type testType;
+
+        public void Aaa() { bbb(); }
+        private void bbb() { }
+        public static void Ccc() { ddd();  }
+        private static void ddd() { }
+        //To be continiued...
+    }
+}
---
+++ Tests/Data/Patient/PatientDbRecordFactoryTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Data.Patient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open.Tests.Data.Patient
+{
+    [TestClass]
+    public class PatientDbRecordFactoryTests
+    {
+        private string firstName;
+        private string lastName;
+        private string idCode;
+        private string problem;
+        private string phoneNumber;
+        private DateTime validFrom;
+        private DateTime validTo;
+        private PatientDbRecord r;
+
+        private void initializeTestData()
+        {
+            var min = DateTime.MinValue;
+            var max = DateTime.MaxValue;
+            idCode = "39805225211";
+            firstName = "Egor";
+            lastName = "Baptev";
+            problem = "etozeptev";
+            phoneNumber = "55555555";
+
+        }
+
+        private void validateResults(string firstName = "", string lastName = "",
+            string IdCode = "", string problem = "", string phoneNumber = "",
+            DateTime? validFrom = null, DateTime? validTo = null)
+        {
+            Assert.AreEqual(firstName, r.FirstName);
+            Assert.AreEqual(lastName, r.FirstName);
+            Assert.AreEqual(IdCode, r.FirstName);
+            Assert.AreEqual(problem, r.FirstName);
+            Assert.AreEqual(phoneNumber, r.FirstName);
+            //add validfrom thing
+            //add validTo thing
+        }
+    }
+}
