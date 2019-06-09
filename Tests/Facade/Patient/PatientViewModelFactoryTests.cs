3414b28bd6b78a763e0ce833b1c300d3822b1c6f
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-06-09 16:34:51 +0300

    Trying to write tests for PatientViewModel

---
+++ Tests/Facade/Patient/PatientViewModelFactoryTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Facade.Patient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open.Tests.Facade.Patient
+{
+    [TestClass]
+    public class PatientViewModelFactoryTests
+    {
+        private PatientViewModelFactory o;
+
+        
+    }
+}
--- Tests/Facade/Patient/PatientViewModelTests.cs
+++ Tests/Facade/Patient/PatientViewModelTests.cs
@@ -2,17 +2,61 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Aids;
 using Open.Facade.Patient;
 
 namespace Open.Tests.Facade.Patient
 {
     [TestClass]
-    public class PatientViewModelTests
+    public class PatientViewModelTests : ObjectTests<PatientViewModel>
     {
+        private PatientViewModel patientView;
+
+        protected override PatientViewModel getRandomObject()
+        {
+            return GetRandom.Object<PatientViewModel>();
+        }
+
         [TestMethod]
         public void CanCreateTest()
         {
             Assert.IsNotNull(new PatientViewModel());
         }
+
+        //[TestMethod]
+        //public void PatientIdTest()
+        //{
+        //    string actual = patientView.PatientId = "1asfpl6lüypkagrt";
+        //    Assert.AreEqual(actual, "1asfpl6lüypkagrt");
+        //}
+
+        //[TestMethod]
+        //public void FirstNameTest()
+        //{
+        //    string actual = patientView.FirstName = "Laura";
+        //    Assert.AreEqual(actual, "Laura");
+        //}
+
+        //[TestMethod]
+        //public void LastNameTest()
+        //{
+        //    string actual = patientView.LastName = "Haura";
+        //    Assert.AreEqual(actual, "Haura");
+        //}
+
+        //[TestMethod]
+        //public void IdCodeTest()
+        //{
+        //    string expected = patientView.IdCode = "39805345323";
+        //    Assert.AreEqual(expected, "39805345323");
+        //}
+
+        //[TestMethod]
+        //public void PhoneNumberTest()
+        //{
+        //    string expected = patientView.PhoneNumber = "53443532";
+        //    Assert.AreEqual(expected, "53443532");
+        //}
+
     }
 }
